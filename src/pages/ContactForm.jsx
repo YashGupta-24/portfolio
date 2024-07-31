af56b80cb7ef8212921370b37534b3ff76b7b067
YashGupta-24 <yashgupta.241103@gmail.com>
2024-07-31 16:24:05 +0530

    form response update

--- src/pages/ContactForm.jsx
+++ src/pages/ContactForm.jsx
@@ -3,7 +3,6 @@ import Navbar from '../sections/Navbar'
 import Footer from '../sections/Footer';
 import picture from '../assets/picture.jpg'
 import ScrollToTop from '../ScrollToTop';
-import { HiArrowRight } from "react-icons/hi";
 
 function ContactForm() {
 
@@ -13,18 +12,18 @@ function ContactForm() {
   const [desc, setDesc] = useState('');
   const [budget, setBudget] = useState(0.0);
 
-  const submitForm = () =>{
-    const script='https://script.google.com/macros/s/AKfycbykGCU8T0e0tMTlx-iQm740-H0DTyCeit5S9gmGEXFqdM4tC9yeXwc0QL_G_qBzNr9W4w/exec'
-
+  const submitForm =(e)=>{
     const form=document.forms['response-form'];
+    e.preventDefault()
+    const formData=new FormData(form);
+    fetch("https://script.google.com/macros/s/AKfycbykGCU8T0e0tMTlx-iQm740-H0DTyCeit5S9gmGEXFqdM4tC9yeXwc0QL_G_qBzNr9W4w/exec",{
+      method: "POST",
+      body: formData
+    }).then((response)=>response.json())
+      .then((data)=>{console.log(data)})
+      .catch((error)=>console.log(error))
 
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      fetch(script, { method: 'POST', body: new FormData(form)})
-      .then(response => alert("Thank you! your form is submitted successfully." ))
-      .then(() => { reset(); })
-      .catch(error => console.error('Error!', error.message))
-  })
+    reset();
   }
 
   const reset=()=>{
@@ -43,7 +42,7 @@ function ContactForm() {
         <img src={picture} alt="" className='h-20 w-20 sm:h-24 sm:w-24 my-4' />
         <h1 className='text-4xl my-5 font-heading text-heading'>Let's Work Together</h1>
         <p className='my-8 font-body font-semibold'>Please answer the following questions so I can get an idea of what you're looking for.</p>
-        <form name='response-form'>
+        <form name='response-form' onSubmit={(e)=>submitForm(e)}>
           <h1 className='font-body font-semibold'>What's your email</h1>
           <input
             className='border-2 border-heading w-full sm:w-1/3 my-3 h-8 rounded-md font-body font-semibold outline-none focus:border-[3px] focus:border-tertiary pl-1'
@@ -86,9 +85,10 @@ function ContactForm() {
             id='budget' 
             name='budget'
             min={0}
+            step={0.01}
             value={budget}
             onChange={(e) => setBudget(e.target.value)}/>
-          <button type="submit" className='sm:w-1/6 p-1 flex gap-2 items-center justify-center rounded-lg bg-tertiary text-white font-semibold mt-3 font-heading cursor-pointer hover:bg-hover transition-all duration-200' value='Submit' id='submit' onClick={submitForm}>Submit <HiArrowRight /></button>
+          <input type="submit" className='sm:w-1/6 p-1 flex gap-2 items-center justify-center rounded-lg bg-tertiary text-white font-semibold mt-3 font-heading cursor-pointer hover:bg-hover transition-all duration-200' value='Submit' id='submit'/>
         </form>
       </div>
       <Footer />
