9db9853ee5753e54acc97b2a883a780a43f0bfce
YashGupta-24 <yashgupta.241103@gmail.com>
2025-08-26 13:35:10 +0530

    resume update

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -15,12 +15,12 @@ function Navbar() {
     <>
       <div className='h-20 w-3/4 flex justify-between items-center fixed top-0 z-50 rounded-sm px-3 backdrop-blur-lg text-heading'>
         <NavLink to={"/"} className='font-logo text-lg md:text-3xl font-bold'>YG</NavLink>
-        <div className='w-1/2 lg:w-2/5 md:flex justify-evenly hidden font-heading font-bold'>
+        <div className='w-2/3 lg:w-1/2 md:flex justify-evenly hidden font-heading font-bold'>
           <NavLink to={"/"} style={navLinkStyle} className="hover:text-tertiary">Home</NavLink>
           <NavLink to={"/works"} style={navLinkStyle} className="hover:text-tertiary">Work</NavLink>
           <NavLink to={"/about"} style={navLinkStyle} className="hover:text-tertiary">About</NavLink>
           <NavLink to={"/experience"} style={navLinkStyle} className="hover:text-tertiary">Experience</NavLink>
-          <a href="https://www.canva.com/design/DAF6KUUNhJI/Ulwm3m7yiTVS-2s63dUORA/view?utm_content=DAF6KUUNhJI&utm_campaign=designshare&utm_medium=link&utm_source=editor" target='blank' className="hover:text-tertiary">Resume</a>
+          <a href="https://www.canva.com/design/DAGxJyAObZU/rjPiB10tA1IUqVliqwge5A/edit?utm_content=DAGxJyAObZU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target='blank' className="hover:text-tertiary">Resume</a>
         </div>
         <div className='md:hidden'>
           <button onClick={show}><HiBarsArrowDown /></button>
@@ -32,7 +32,7 @@ function Navbar() {
           <NavLink to={"/works"} style={navLinkStyle} className="hover:text-tertiary">Work</NavLink>
           <NavLink to={"/about"} style={navLinkStyle} className="hover:text-tertiary">About</NavLink>
           <NavLink to={"/experience"} style={navLinkStyle} className="hover:text-tertiary">Experience</NavLink>
-          <a href="https://www.canva.com/design/DAF6KUUNhJI/Ulwm3m7yiTVS-2s63dUORA/view?utm_content=DAF6KUUNhJI&utm_campaign=designshare&utm_medium=link&utm_source=editor" target='blank' className="hover:text-tertiary">Resume</a>
+          <a href="https://www.canva.com/design/DAGxJyAObZU/rjPiB10tA1IUqVliqwge5A/edit?utm_content=DAGxJyAObZU&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target='blank' className="hover:text-tertiary">Resume</a>
         </div>
       </div>
     </>
